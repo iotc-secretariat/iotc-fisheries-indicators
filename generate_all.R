ed76c7223dd271d8d99b0ef382984c7d59afe460
Emmanuel Chassot <EChassot@iotc.local>
2022-10-24 11:55:05 +0400

    Finalisation of PPTX

--- generate_all.R
+++ generate_all.R
@@ -4,8 +4,8 @@ source("00_CORE.R")
 setwd("..")
 
 # PPTX
-# render("rmd/FAD_INDICATORS_DASHBOARD_PPTX.Rmd",
-#         output_format = powerpoint_presentation(reference_doc = "../templates/ppt_template.potx", slide_level = 2),
-#         output_dir    = "outputs/"
-# )
+render("rmd/00_PPTX.Rmd",
+        output_format = powerpoint_presentation(reference_doc = "../templates/ppt_template.potx", slide_level = 2),
+        output_dir    = "outputs/"
+)
 
